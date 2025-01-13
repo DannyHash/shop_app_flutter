79b6bec7eec8b3256d5779359393aebd53c64675
Danny-Hash <dannyhash0x1@gmail.com>
2025-01-13 05:25:54 +0100

    Addeded search functionality to HomePage with styled input field and updated theme for input decorations

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -5,16 +5,42 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    const border = OutlineInputBorder(
+      borderSide: BorderSide(
+        color: Color.fromRGBO(225, 225, 225, 1),
+      ),
+      borderRadius: BorderRadius.horizontal(
+        left: Radius.circular(50),
+      ),
+    );
     return Scaffold(
       body: SafeArea(
         child: Column(
           children: [
-            Text(
-              'Shoes\nCollection',
-              style: TextStyle(
-                fontSize: 35,
-                fontWeight: FontWeight.bold,
-              ),
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Text(
+                    'Shoes\nCollection',
+                    style: TextStyle(
+                      fontSize: 35,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: 'Search',
+                      prefixIcon: Icon(Icons.search),
+                      border: border,
+                      enabledBorder: border,
+                      focusedBorder: border,
+                    ),
+                  ),
+                ),
+              ],
             ),
           ],
         ),

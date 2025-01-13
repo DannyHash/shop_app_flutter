1eef3c47a04d0a1191566d12bbb9ff02ea45fc5b
Danny-Hash <dannyhash0x1@gmail.com>
2025-01-13 05:56:59 +0100

    Addeded filter chips to HomePage for product categorization

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -3,6 +3,13 @@ import 'package:flutter/material.dart';
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
+  final List<String> filters = const [
+    'All',
+    'Addidas',
+    'Nike',
+    'Bata',
+  ];
+
   @override
   Widget build(BuildContext context) {
     const border = OutlineInputBorder(
@@ -42,6 +49,36 @@ class HomePage extends StatelessWidget {
                 ),
               ],
             ),
+            SizedBox(
+              height: 120,
+              child: ListView.builder(
+                itemCount: filters.length,
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (context, index) {
+                  final filter = filters[index];
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                    child: Chip(
+                      backgroundColor: Color.fromRGBO(245, 247, 249, 1),
+                      side: BorderSide(
+                        color: Color.fromRGBO(245, 247, 249, 1),
+                      ),
+                      label: Text(filter),
+                      labelStyle: TextStyle(
+                        fontSize: 16,
+                      ),
+                      padding: EdgeInsets.symmetric(
+                        horizontal: 20,
+                        vertical: 15,
+                      ),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            )
           ],
         ),
       ),
